ac7163ce694f9ff5e72f0c31924cc5fb1c690d65
Iain Kelly <igkuk7@gmail.com>
2018-09-28 18:36:23 +0100

    fixing http server

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+ 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
